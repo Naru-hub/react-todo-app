966ad5868d23bed72b55ed1416ac2018d99e8e1c
Naru-hub <1208ny0324@gmail.com>
2022-02-09 14:37:30 +0900

    todo作成 削除機能(localstrageなし）

--- src/App.jsx
+++ src/App.jsx
@@ -1,115 +1,86 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function App() {
-  const [todos, setTodos] = useState(() => {
-    const savedTodos = localStorage.getItem("todos");
-    if (savedTodos) {
-      return JSON.parse(savedTodos);
-    } else {
-      return [];
-  }
-});
+const App = () => {
+  const [todos, setTodos] = useState([]);
 
+  //入力値を追跡
   const [todo, setTodo] = useState("");
 
+  //編集モードかどうかをbool値で判断
   const [isEditing, setIsEditing] = useState(false);
+
+  //編集するToDoアイテムがわかるように設定するオブジェクトの状態
   const [currentTodo, setCurrentTodo] = useState({});
 
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
 
-  function handleInputChange(e) {
+  //formの入力値を受け取る関数
+  const handleInputChange = (e) => {
     setTodo(e.target.value);
   }
 
-  function handleEditInputChange(e) {
-    setCurrentTodo({...currentTodo, text: e.target.value });
-    console.log(currentTodo);
-  }
-
-  function handleFormSubmit(e) {
-    e.preventDefault();
-
+  //todo作成の関数
+  const handleFormSubmit = (e) => {
     if (todo !== "") {
       setTodos([
-        ...todos,
+        ...todos,   //状態の現在の値をコピー
         {
           id: todos.length + 1,
-          text: todo.trim()
+          text: todo.trim(),
+          status: "notStarted"
         }
       ]);
     }
+    //formの値を空にする
     setTodo("");
   }
 
-  function handleEditFormSubmit(e) {
-    e.preventDefault();
 
-    handleUpdateTodo(currentTodo.id, currentTodo);
-  }
-
-  function handleDeleteClick(id) {
+  //todosの配列からtodoを削除する関数
+  const handleDeleteClick = (id) => {
+    //todo.idとidが違う残りのtodoを抽出して返す
     const removeItem = todos.filter((todo) => {
       return todo.id !== id;
     });
+    //removeItemは新しい配列を返す
     setTodos(removeItem);
   }
 
-  function handleUpdateTodo(id, updatedTodo) {
-    const updatedItem = todos.map((todo) => {
-      return todo.id === id ? updatedTodo : todo;
-    });
-    setIsEditing(false);
-    setTodos(updatedItem);
-  }
-
-  function handleEditClick(todo) {
-    setIsEditing(true);
-    setCurrentTodo({ ...todo });
+  //編集入力の値を取得し、新しい状態を設定する関数
+  const handleEditInputChange = (e) => {
+    //新しい状態の値を、現在編集入力ボックスにあるものに設定
+    setCurrentTodo({ ...currentTodo, text: e.target.value });
+    console.log(currentTodo);
   }
 
   return (
-    <div className="App">
-      {isEditing ? (
-        <form onSubmit={handleEditFormSubmit}>
-          <h2>Edit Todo</h2>
-          <label htmlFor="editTodo">Edit todo: </label>
-          <input
-            name="editTodo"
-            type="text"
-            placeholder="todoを編集する"
-            value={currentTodo.text}
-            onChange={handleEditInputChange}
-          />
-          <button type="submit">更新</button>
-          <button onClick={() => setIsEditing(false)}>キャンセル</button>
-        </form>
-      ) : (
-      <form onSubmit={handleFormSubmit}>
+    <>
+      <div>
         <h2>Add Todo</h2>
-        <label htmlFor="todo">ToDo : </label>
+        <label htmlFor="todo" className="mr-2">ToDo : </label>
         <input
         name="todo"
         type="text"
         placeholder="新しいtodoを作成する"
         value={todo}
-        onChange={handleInputChange}
+        onChange={handleInputChange}  //handleInputChange formの入力値を受け取る関数
         />
-        <button type="submit" class="btn btn-info mx-2">作成</button>
-      </form>
-      )}
+        <button onClick={() => handleFormSubmit(todo.id)} type="submit" className="btn btn-info mx-2">作成</button>
+      </div>
 
-    <ul className="todo-list my-2">
-      {todos.map((todo) => (
-        <li key={todo.id} className="my-2">
-          {todo.text}
-          <button class="btn btn-success ml-5" onClick={() => handleEditClick(todo)}>編集</button>
-          <button class="btn btn-danger mx-2"onClick={() => handleDeleteClick(todo.id)}>削除</button>
-        </li>
-      ))}
-    </ul>
-  </div>
+      <div>
+        <h1>ToDo List</h1>
+        <ul>
+          {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+            <button className="btn btn-danger mx-2" onClick={() => handleDeleteClick(todo.id)}>削除</button>
+          </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
-}
+};
+
+export default App;
